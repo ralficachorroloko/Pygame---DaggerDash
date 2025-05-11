55792688ca60e291d70f031ed9f11ac7d51a7225
Rafael Capozzielli <capozzielli.rafael@gmail.com>
2025-05-11 00:59:08 -0300

    Classes - Parede 1.0

--- classes.py
+++ classes.py
@@ -9,7 +9,7 @@ class Player:
         self.rect = self.imagem.get_rect(topleft=(x, y))
         self.velocidade = velocidade 
 
-    def desenhar_player(self, tela):
+    def desenhar(self, tela):
         tela.blit(self.imagem, self.rect)
 
     def mover(self, dx, dy):
@@ -17,4 +17,17 @@ class Player:
         self.rect.y += dy * self.velocidade
         self.rect.x = max(0, min(self.rect.x, WIDTH - self.rect.width))
         self.rect.y = max(0, min(self.rect.y, HEIGHT - self.rect.height))
+
+class Parede:
+    def __init__(self, x, y, largura, altura):
+        self.x = x
+        self.y = y
+        self.largura = largura
+        self.altura = altura
+        self.rect = pygame.Rect(x, y, largura, altura)
+    
+    def desenhar(self, tela, mostrar_hitbox=False):
+        if mostrar_hitbox == True:
+            pygame.draw.rect(tela, (255, 0, 0), self.rect)
+
         
--- testes.py
+++ testes.py
@@ -6,6 +6,7 @@ from config import *
 
 def tela_teste(tela):
     jogador = Player(400, 300, "idle.png", 10, (50, 50))
+    parede = Parede(300, 400, 100, 300)
 
     pygame.mixer.init()
     pygame.mixer.music.load(path.join('8bitmusic.mp3'))
@@ -29,10 +30,12 @@ def tela_teste(tela):
             jogador.mover(0, 1)
         if keys[pygame.K_LEFT]:
             jogador.mover(-1, 0)
-            
+
         if keys[pygame.K_RIGHT]:
             jogador.mover(1, 0)
 
         tela.fill(BLACK)
-        jogador.desenhar_player(tela)
+        jogador.desenhar(tela)
+        parede.desenhar(tela, True)
+
         pygame.display.flip()
